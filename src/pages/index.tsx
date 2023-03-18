034a6e8398889cf4baff6ac13dbcf11f5dfd9ab3
MathieuGery <mathieu.gery@epitech.eu>
2023-03-18 13:34:04 +0100

    v2

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+import NavBar from '@/components/NavBar'
+import Footer from '@/components/Footer'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Claire's portfolio</title>
+        <meta name="description" content="Claire's portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <NavBar />
+      </main>
+      <Footer />
+    </>
+  )
+}
