7b33057d2cc8a8d75febb884c053b3eb96893ba1
cgmxcl <101254583+cgmxcl@users.noreply.github.com>
2023-03-23 13:57:40 +0100

    Update projects.ts

--- src/projects/projects.ts
+++ src/projects/projects.ts
@@ -9,7 +9,7 @@ export const projects = [
     imageSrc: '/LFAR_portrait.png',
     images: [{
       source:
-        '/flyer_paysage2.png',
+        '/flyer_paysage2.jpg',
     },
     {
       source:
