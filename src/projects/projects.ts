73bd725e47b7d843cac8a409f5acfd448603ff24
cgmxcl <101254583+cgmxcl@users.noreply.github.com>
2023-03-25 12:36:20 +0100

    Update projects.ts

--- src/projects/projects.ts
+++ src/projects/projects.ts
@@ -54,19 +54,8 @@ export const projects = [
     {
       source:
         '/GERY _Claire _Packaging_ DG3A 12.jpg',
-    },
-    {
-      source:
-        '/GERY _Claire _Packaging_ DG3A 4.jpg',
-    },
-    {
-      source:
-        '/GERY _Claire _Packaging_ DG3A 6.jpg',
-    },
-    {
-      source:
-        '/GERY _Claire _Packaging_ DG3A 8.jpg',
     },],
+     
     imageAlt:
       'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
   },
