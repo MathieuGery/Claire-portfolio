c94969e04bd5e8fc07f652859c01d4a996023998
cgmxcl <101254583+cgmxcl@users.noreply.github.com>
2024-01-21 17:16:01 +0100

    Update a_propos.ts

--- src/projects/a_propos.ts
+++ src/projects/a_propos.ts
@@ -1,6 +1,6 @@
 export const a_propos = {
     name: "Claire Gery",
-    tagLine: " Commodo amet fugiat excepteur sunt qui ea elit cupidatat ullamco consectetur ipsum elit consequat. Elit sunt proident ea nulla ad nulla dolore ad pariatur tempor non. Sint veniam minim et ea.",
+    tagLine: " Hello ! Moi c'est Claire. Passionnée par le graphisme, l'illustration et le design en général, c'est tout naturellement que je me suis lancée dans des études de graphisme. Je suis actuellement en M1 direction artistique 360° à l'ECV de Lille et je recherche un stage de 4 mois minimum à partir du 1er avril 2024.",
     baseLine: "Etudiante en machn truc design trop bo",
     title: "Title",
     description: "Quasi est quaerat. Sit molestiae et. Provident ad dolorem occaecati eos iste. Soluta rerum quidem minus ut molestiae velit error quod. Excepturi quidem expedita molestias quas.",
