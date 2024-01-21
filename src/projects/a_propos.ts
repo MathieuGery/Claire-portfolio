5c4ba00931beabd9666b133c4d7937d0a74273bb
MathieuGery <mathieu.gery@epitech.eu>
2024-01-21 19:05:31 +0100

    update

--- src/projects/a_propos.ts
+++ src/projects/a_propos.ts
@@ -3,8 +3,7 @@ export const a_propos = {
     tagLine: " Hello ! Moi c'est Claire. Passionnée par le graphisme, l'illustration et le design en général, c'est tout naturellement que je me suis lancée dans des études de graphisme. Je suis actuellement en M1 direction artistique 360° à l'ECV de Lille et je recherche un stage de 4 mois minimum à partir du 1er avril 2024.",
     baseLine: "",
     title: "Title",
-    description: "Curieuse et admirative, je n'hésite pas à exprimer ma créativité avec différentes techniques telles que la linogravure, la sérigraphie, l'art digital, la peinture, le tricot ou encore le crochet.",
-    description2: "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat. Quasi aperiam sit non sit neque reprehenderit.",
+    description: "Affiche, flyer, packaging, charte graphique, logo, webdesign... J'aime concevoir et donner vie à mes visuels quel que soit le support. Je suis également curieuse, j'adore découvrir de nouvelles techniques et n'hésite pas à m'en servir dans mes créations. Pour télécharger mon cv, c'est par ici !",
     images: [{
         source: "/flyer_portrait_3.png"
     }, {
