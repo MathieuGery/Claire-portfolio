4901981e0afaef6fde7fb2c624eb79fdd5cee055
cgmxcl <101254583+cgmxcl@users.noreply.github.com>
2024-01-24 19:31:30 +0100

    update

--- src/projects/a_propos.ts
+++ src/projects/a_propos.ts
@@ -2,7 +2,7 @@ export const a_propos = {
     name: "Claire Gery",
     tagLine: " Hello ! Moi c'est Claire. Passionnée par le graphisme, l'illustration et le design en général, c'est tout naturellement que je me suis lancée dans des études de graphisme. Je suis actuellement en M1 direction artistique 360° à l'ECV de Lille et je recherche un stage de 4 mois minimum à partir du 1er avril 2024.",
     baseLine: "",
-    title: "Title",
+    title: "Découvrir mon univers",
     description: "Affiche, flyer, packaging, charte graphique, logo, webdesign... J'aime concevoir et donner vie à mes visuels quel que soit le support. Je suis également curieuse, j'adore découvrir de nouvelles techniques et n'hésite pas à m'en servir dans mes créations. Pour télécharger mon cv, c'est par ici !",
     images: [{
         source: "/les-fou-a-reaction/flyer_portrait_3.jpeg"
